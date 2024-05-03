639fb1765ad810b97d6b32486f5e076ec1224ace
Sanjay Pahari <sanjay.pahari098@gmail.com>
2024-05-03 15:03:25 +0545

    mobile responsive done for few section

--- src/Pages/KnowusSection/KnowUs.styled.ts
+++ src/Pages/KnowusSection/KnowUs.styled.ts
@@ -1,4 +1,64 @@
+
 import styled from "styled-components";
+
+const HeaderPictureContainer = styled.div`
+    position: relative;
+`;
+
+const ExpandButton = styled.button`
+    margin-top: 10px;
+    z-index: 2;
+    cursor: pointer;
+    background: none;
+    border: none;
+    opacity: 0;
+    color: black;
+    transition: opacity 0.3s ease;
+`;
+
+const PictureSection = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 20px;
+    gap: 20px;
+    cursor: pointer;
+
+    &:hover {
+        ${ExpandButton} {
+            opacity: 1;
+        }
+    }
+`;
+
+const ProjectsPicture = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 10px;
+    padding-top: 10px;
+`;
+
+const ProjectDescription = styled.div`
+display: flex;
+    flex-direction: row;
+    gap: 40px;
+`;
+const Picture = styled.img`
+    height: 10vw;
+    width: 10vw;
+
+    @media (min-width: 768px) {
+        height: 50px;
+        width: 50px;
+    }
+`;
+const ProjectName = styled.h4`
+
+`;
+
+const ProjectDomain = styled.p``;
 
 const Container = styled.div`
     display: flex;
@@ -29,7 +89,12 @@ const Services = styled.div`
         align-items: center;
     }
 `;
+const  ProjectDescriptioncontainer = styled.div`
+display: flex;
+    flex-direction: column;
+    gap : 0;
 
+`;
 const PictureText = styled.div`
     position: absolute;
     top: 50%;
@@ -53,55 +118,4 @@ const HeaderPicture = styled.img`
     }
 `;
 
-const ExpandButton = styled.button`
-    position: absolute;
-    bottom: 10%;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 2;
-    cursor: pointer;
-    background: none;
-    border: none;
-    opacity: 0;
-    transition: opacity 0.3s ease;
-
-  
-    
-`;
-
-
-const PictureSection = styled.div`
-    position: relative;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 20px;
-    gap: 20px;
-    cursor: pointer;
-
-    &:hover {
-        ${ExpandButton} {
-            opacity: 1;
-        }
-    }
-`;
-
-const ProjectsPicture = styled.div`
-    flex-direction: row;
-    align-items: center;
-    gap: 10px;
-    padding-top: 10px;
-`;
-
-const Picture = styled.img`
-    height: 10vw;
-    width: 10vw;
-
-    @media (min-width: 768px) {
-        height: 50px;
-        width: 50px;
-    }
-`;
-
-export { Container, PictureText, Picture, Title, Services, PictureSection, HeaderPicture, ExpandButton, ProjectsPicture };
+export { Container, ProjectDescriptioncontainer ,ProjectName, ProjectDomain ,ProjectDescription , PictureText, Picture, Title, Services, PictureSection, HeaderPicture, ExpandButton, ProjectsPicture, HeaderPictureContainer };
--- src/Pages/KnowusSection/Knowus.tsx
+++ src/Pages/KnowusSection/Knowus.tsx
@@ -1,5 +1,5 @@
-import  { useState } from "react";
-import { Container, Title, Services, PictureSection, PictureText, Picture, ProjectsPicture, HeaderPicture, ExpandButton } from "./KnowUs.styled";
+import { useState } from "react";
+import { Container, Title, Services,  ProjectName,ProjectDescriptioncontainer, ProjectDomain ,ProjectDescription ,  HeaderPictureContainer ,PictureSection, PictureText, Picture, ProjectsPicture, HeaderPicture, ExpandButton } from "./KnowUs.styled";
 
 import Revel from "../../assets/OurServicesPicture/crowd.jpg";
 import Academics from "../../assets/OurServicesPicture/acadenics.jpg";
@@ -7,11 +7,25 @@ import Hospitality from "../../assets/OurServicesPicture/hospitality.jpg";
 import RealEstate from "../../assets/OurServicesPicture/realestate.jpg";
 import PlaceholderPicture from "../../assets/OurServicesPicture/projectpicture.jpg";
 
+interface ExpandedSectionsState {
+    revel: boolean;
+    hospitality: boolean;
+    realEstate: boolean;
+    academics: boolean;
+}
 const KnowUs = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
+    const [expandedSections, setExpandedSections] = useState<ExpandedSectionsState>({
+        revel: false,
+        hospitality: false,
+        realEstate: false,
+        academics: false,
+    });
 
-    const toggleExpand = () => {
-        setIsExpanded(!isExpanded);
+    const toggleExpand = (sectionName: keyof ExpandedSectionsState) => {
+        setExpandedSections((prevState: ExpandedSectionsState) => ({
+            ...prevState,
+            [sectionName]: !prevState[sectionName],
+        }));
     };
 
     return (
@@ -19,48 +33,183 @@ const KnowUs = () => {
             <Title>Get to know us!</Title>
             <Services>
                 <PictureSection>
-                    <HeaderPicture src={Revel} />
-                    <PictureText>Revel</PictureText>
-                    <ExpandButton onClick={toggleExpand}>Click To Expand</ExpandButton>
-                    <ProjectsPicture >
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                    </ProjectsPicture>
+                    <HeaderPictureContainer>
+                        <HeaderPicture src={Revel} />
+                        <PictureText>Revel</PictureText>
+                    </HeaderPictureContainer>
+                    <ExpandButton onClick={() => toggleExpand("revel")}>
+                        {expandedSections.revel ? "Collapse" : "Click To Expand"}
+                    </ExpandButton>
+                    {expandedSections.revel && (
+                        <ProjectsPicture>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                        </ProjectsPicture>
+                    )}
                 </PictureSection>
                 <PictureSection>
+                    <HeaderPictureContainer>
                     <HeaderPicture src={Hospitality} />
                     <PictureText>Hospitality</PictureText>
-                    <ExpandButton>Click To Expand</ExpandButton>
-                    <ProjectsPicture>
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                    </ProjectsPicture>
+                    </HeaderPictureContainer>
+                    <ExpandButton onClick={() => toggleExpand("hospitality")}>
+                        {expandedSections.hospitality ? "Collapse" : "Click To Expand"}
+                    </ExpandButton>
+                    {expandedSections.hospitality && (
+                        <ProjectsPicture>
+                            <ProjectDescription>
+
+                            <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                        </ProjectsPicture>
+                    )}
                 </PictureSection>
                 <PictureSection>
+                    <HeaderPictureContainer>
+
                     <HeaderPicture src={RealEstate} />
                     <PictureText>RealEstate</PictureText>
-                    <ExpandButton>Click To Expand</ExpandButton>
-                    <ProjectsPicture>
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                    </ProjectsPicture>
+                    </HeaderPictureContainer>
+                    <ExpandButton onClick={() => toggleExpand("realEstate")}>
+                        {expandedSections.realEstate ? "Collapse" : "Click To Expand"}
+                    </ExpandButton>
+                    {expandedSections.realEstate && (
+                        <ProjectsPicture>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                        </ProjectsPicture>                    )}
                 </PictureSection>
                 <PictureSection>
+                    <HeaderPictureContainer>
                     <HeaderPicture src={Academics} />
                     <PictureText>Academics</PictureText>
-                    <ExpandButton>Click To Expand</ExpandButton>
-                    <ProjectsPicture>
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                        <Picture src={PlaceholderPicture} />
-                    </ProjectsPicture>
+                    </HeaderPictureContainer>
+                    <ExpandButton onClick={() => toggleExpand("academics")}>
+                        {expandedSections.academics ? "Collapse" : "Click To Expand"}
+                    </ExpandButton>
+                    {expandedSections.academics && (
+                        <ProjectsPicture>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                            <ProjectDescription>
+
+                                <Picture src={PlaceholderPicture} />
+                                <ProjectDescriptioncontainer>
+                                    <ProjectName>A</ProjectName>
+                                    <ProjectDomain>www.abc.com</ProjectDomain>
+                                </ProjectDescriptioncontainer>
+                            </ProjectDescription>
+                        </ProjectsPicture>                    )}
                 </PictureSection>
             </Services>
         </Container>
